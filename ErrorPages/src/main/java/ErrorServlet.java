e44505c4a4e438f49a0cbc56de74c84578a922f9
Kanishka Upadhyay <61813125+kanishkaupadhyay@users.noreply.github.com>
2022-07-11 12:00:01 +0530

    Add files via upload

---
+++ ErrorPages/src/main/java/ErrorServlet.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/ErrorServlet", "/error" })
+public class ErrorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ErrorServlet() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			throw new NullPointerException();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
---
+++ ErrorPages/src/main/webapp/500.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Oops!!! Something wrong here!</title>
+</head>
+<body>
+	<h1>Looks like some server error has occured.</h1>
+</body>
+</html>
